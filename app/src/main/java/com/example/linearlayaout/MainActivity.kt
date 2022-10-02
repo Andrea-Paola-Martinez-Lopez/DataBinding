e359d14bb98bfcc5ba087c33a465d83340218b9c
GiovanniAdb <s19120147@alumnos.itsur.edu.mx>
2022-10-02 14:56:57 -0500

    Recursos e Interacción 2

--- app/src/main/java/com/example/linearlayaout/MainActivity.kt
+++ app/src/main/java/com/example/linearlayaout/MainActivity.kt
@@ -1,58 +1,56 @@
 package com.example.linearlayaout
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.linearlayaout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Giovanni de Jesus Alejo Calderon")
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-    findViewById<Button>(R.id.done_button).setOnClickListener {
-        addNickname(it)
-    }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+        findViewById<TextView>(R.id.name_text).setOnClickListener {
             updateNickname(it)
         }
     }
-
-
-    fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
-
-        //Hide the keyboard
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+    private fun addNickname(view: View) {
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nameText.visibility = View.VISIBLE
+        }
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
-    fun updateNickname(view: View) {
+    private fun updateNickname(view: View) {
         val editText = findViewById<EditText>(R.id.nickname_edit)
         val doneButton = findViewById<Button>(R.id.done_button)
-         editText.visibility = View.VISIBLE
-         doneButton.visibility = View.VISIBLE
-         view.visibility = View.GONE
-
-        //Set the focus to the edit text
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
         editText.requestFocus()
-
-        //Show the keyborad
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(editText,0)
+        imm.showSoftInput(editText, 0)
     }
-
     fun clickHandlerFunction(viewThatIsClicked: View) {}
 }
---
+++ app/src/main/java/com/example/linearlayaout/MyName.kt
@@ -0,0 +1,6 @@
+package com.example.linearlayaout
+
+/*class MyName {
+  }
+*/
+    data class MyName(var name: String = "", var nickname: String = "")
